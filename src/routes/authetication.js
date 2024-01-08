25497b995a19dd24efa44e79c1600700eb8ead7b
Yeampier2022 <yeampierjesusa@gmail.com>
2024-01-08 16:35:23 -0400

    feat: add form signup

--- src/routes/authetication.js
+++ src/routes/authetication.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/signup', (req, res) => {
+    res.render('auth/signup')
+})
+
+router.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.send("recibido")
+})
+
 module.exports = router
