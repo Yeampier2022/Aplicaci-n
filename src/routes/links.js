7a557b24d1ba529ac3ae26e3ca74805733c3ecff
Yeampier2022 <yeampierjesusa@gmail.com>
2024-01-09 13:56:21 -0400

    feat: guardian routes

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const {isLoggedIn}= require('../lib/auth')
 
 const pool = require('../database')
 
-router.get('/add', (req, res) => {
+router.get('/add', isLoggedIn, (req, res) => {
     res.render('links/add')
 })
 
-router.post('/add', async (req, res) => {
+router.post('/add',  isLoggedIn, async (req, res) => {
     const { title, url, description } = req.body
     const newLink = {
         title,
@@ -19,25 +20,25 @@ router.post('/add', async (req, res) => {
     res.redirect('/links')
 })
 
-router.get('/', async (req, res) => {
+router.get('/',  isLoggedIn, async (req, res) => {
     const links = await pool.query('SELECT * FROM links')
     res.render('links/list', { links })
 })
 
 
-router.get('/delete/:id', async (req, res) => {
+router.get('/delete/:id',  isLoggedIn, async (req, res) => {
     const { id } = req.params
     await pool.query('DELETE FROM links WHERE ID = ?', [id])
     req.flash('success', 'Link Removed Successfully');
     res.redirect('/links')
 })
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id',  isLoggedIn, async (req, res) => {
     const { id } = req.params
     const links = await pool.query('SELECT * FROM links WHERE id = ?', [id])
     res.render('links/edit', { link: links[0] })
 })
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id',  isLoggedIn, async (req, res) => {
     const { id } = req.params
     const { title, description, url } = req.body
     const newLink = {
