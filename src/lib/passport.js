43849ece254c2de98295beb2022eb87e07c6f6cb
Yeampier2022 <yeampierjesusa@gmail.com>
2024-01-09 11:53:45 -0400

    feat: login messages

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,6 +4,29 @@ const LocalStrategy = require('passport-local').Strategy
 
 const pool = require('../database')
 const helpers = require('../lib/helpers')
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, async (req, username, password, done) => {
+     const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username])
+     if (rows.length > 0) {
+      const user = rows[0]
+      const validPassword = await helpers.matchPassword(password, user.password)
+      if (validPassword ) {
+        done(null, user, req.flash( 'success', 'Welcome' + user.username))
+      } else {
+        done(null, false, req.flash('message', "incorrect Pasword"))
+      }
+     }
+     else {
+      return done(null, false, req.flash('message', "username does not exits"))
+     }
+  }))
+
+
+
+
 
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
